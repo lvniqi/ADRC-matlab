6a7eb979b19a51dd9054bc962bd95a475ac8d493
lvniqi <lvniqi@gmail.com>
2017-05-18 13:18:40 +0800

    添加 pid nn

---
+++ my_nnpid/pid_nn.m
@@ -0,0 +1,10 @@
+classdef pid_nn<bp_nn
+    methods
+        %% 构造函数
+        function obj=pid_nn(input_dim,h1_dim,output_dim)
+            obj = obj@bp_nn(input_dim,h1_dim,output_dim);
+        end
+        %% 前向
+        
+    end
+end
